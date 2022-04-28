7c64d407e08ff9db881e1ad53fe3ec557606fd0f
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-04-28 09:35:38 +0200

    Agario 2D Unity added

---
+++ Agario/Assets/Scripts/RequestServerTime.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestServerTime : MonoBehaviour
+{
+   public GameObject TimeButton;
+   public void SendRequest()
+   {
+      
+   }
+}
