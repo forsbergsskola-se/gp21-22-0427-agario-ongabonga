d4aa4c4d4c5ee66b1119af4ca2f6bdbc66e528b7
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-04-28 16:33:53 +0200

    Added Marc's sample code for UDP as temporary code

--- OpenWord-MMO/Program.cs
+++ OpenWord-MMO/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public static class Program 
+{
+    //This is Marc's Sample code for UDP Server, to be worked on tomorrow
+    static void Main(string[] arguments) 
+    {
+        var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 14411);
+		
+        // We open the Socket, so we can receive Packets
+        var server = new UdpClient(serverEndpoint);
+        while (true) 
+        {
+            // This struct will contain the info of the sender
+            // After calling Receive
+            IPEndPoint clientEndpoint = default;
+            // Here, we receive a message from some client
+            // A ref parameter means, that this function
+            // can change the struct from within the function
+            var response = server.Receive(ref clientEndpoint);
+            Console.WriteLine($"Packet received from: {clientEndpoint} saying: {Encoding.ASCII.GetString(response)}");
+        }
+    }
+}
