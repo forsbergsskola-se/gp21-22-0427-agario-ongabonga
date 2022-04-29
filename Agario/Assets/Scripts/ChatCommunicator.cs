1dccae4fc0ff192ea067a36ff2d178ac8ab7b447
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-04-29 13:30:48 +0200

    Chat communicator tweaks can send message to server

--- Agario/Assets/Scripts/ChatCommunicator.cs
+++ Agario/Assets/Scripts/ChatCommunicator.cs
@@ -12,15 +12,27 @@ public class ChatCommunicator : MonoBehaviour
     {
         chatOutput = FindObjectOfType<TextMeshProUGUI>();
     }
-
+    
     public void SendMessageRequest()
     {
+        //TODO: Fix socketexception on multiple clicks/attempts/requests
+        
         var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 13337);
-        var clientEndpoint = new IPEndPoint(IPAddress.Loopback, 66669);
+        var clientEndpoint = new IPEndPoint(IPAddress.Loopback, 666);
 
         var udpClient = new UdpClient(clientEndpoint);
 
         var msg = Encoding.ASCII.GetBytes(chatOutput.text);
         udpClient.Send(msg, msg.Length, serverEndpoint);
+        
+    }
+
+    public void ReceiveMessage()
+    {
+        var serverEndpoint = new IPEndPoint(IPAddress.Loopback, 13337);
+        var clientEndpoint = new IPEndPoint(IPAddress.Loopback, 666);
+        var udpClient = new UdpClient(clientEndpoint);
+        var response = udpClient.Receive(ref serverEndpoint);
+        chatOutput.text = Encoding.ASCII.GetString(response);
     }
 }
