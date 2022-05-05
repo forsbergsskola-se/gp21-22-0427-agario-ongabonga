15dfbb06982a4d61adc343dd8230120d645d7844
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-05-05 11:51:16 +0200

    Oh no, tiny plater got eaten(forgot to set collider)

--- Agario/Assets/Scripts/Player.cs
+++ Agario/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 {
     public int score;
     float sizeManipulator;
-    private Collider myCollider;
+    private Collider2D myCollider;
     
     
     public void ControlSize()
@@ -18,9 +18,11 @@ public class Player : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D other)
     {
+        myCollider = GetComponent<Collider2D>();
         if (myCollider.bounds.Contains(other.bounds.min) && myCollider.bounds.Contains(other.bounds.max))
         {
             //TODO: NOM NOM. But first, LUNCH NOM NOM
+            Debug.Log("Oh No, tiny player got eaten :O!");
         }
     }
 }
