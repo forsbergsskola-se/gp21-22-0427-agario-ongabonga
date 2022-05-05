a070e44ef7f3f19eaaf5b7a9987a16368949a1ad
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-05-05 11:45:11 +0200

    Check if we overtake other playah

--- Agario/Assets/Scripts/Player.cs
+++ Agario/Assets/Scripts/Player.cs
@@ -7,12 +7,21 @@ public class Player : MonoBehaviour
 {
     public int score;
     float sizeManipulator;
+    private Collider myCollider;
     
     
     public void ControlSize()
     {
         sizeManipulator = 1 + score * 0.1f;
         GetComponentInParent<Transform>().localScale = new Vector3(sizeManipulator, sizeManipulator, 1);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (myCollider.bounds.Contains(other.bounds.min) && myCollider.bounds.Contains(other.bounds.max))
+        {
+            //TODO: NOM NOM. But first, LUNCH NOM NOM
+        }
     }
 }
 
