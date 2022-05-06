a3c2ad0e0fa9074c4dc1ab55ee55e018a967af9e
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-05-06 10:11:25 +0200

    Tcp Client for agario following Marcs example

---
+++ AgarioServer/AgarioMatch.cs
@@ -0,0 +1,14 @@
+using System.Net.Sockets;
+
+namespace AgarioServer;
+
+public class AgarioMatch
+{
+   public TcpClient Onga;
+   public TcpClient Bonga;
+
+   public void Start()
+   {
+      
+   }
+}
--- AgarioServer/Program.cs
+++ AgarioServer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace AgarioServer;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var tcpListener = new TcpListener(IPAddress.Any, 1337);
+        tcpListener.Start();
+
+        Console.WriteLine("Listening on: " + tcpListener.LocalEndpoint);
+
+        AgarioMatch match = null;
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for players to connect");
+            var tcpClient = tcpListener.AcceptTcpClient();
+
+            Console.WriteLine($"{tcpClient.Client.RemoteEndPoint} has achieved maximum connectitude, baby!");
+
+            if (match == null)
+            {
+                Console.WriteLine("Onga here! Awaiting arrival of Bonga");
+                match = new AgarioMatch
+                {
+                    Onga = tcpClient
+                };
+            }
+            else
+            {
+                Console.WriteLine("Onga here first now Bonga here 2. Time for OngaBonga");
+                match.Bonga = tcpClient;
+                new Thread(match.Start).Start();
+                match = null;
+            }
+        }
+    }
+}
