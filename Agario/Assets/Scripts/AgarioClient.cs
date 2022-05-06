d516c783e46e81bbd99cef0feec4856a28fc88eb
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-05-06 15:29:35 +0200

    added the URP for fun

--- Agario/Assets/Scripts/AgarioClient.cs
+++ Agario/Assets/Scripts/AgarioClient.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Threading;
 using Messages;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class AgarioClient : MonoBehaviour{
   public event Action<MatchInfoMessage> matchInfoMessageRecieved; 
