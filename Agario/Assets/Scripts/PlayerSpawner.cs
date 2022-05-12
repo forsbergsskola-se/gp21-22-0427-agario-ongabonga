6037be506da1183b402f857fec3fa52bfe91791e
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-05-12 12:19:12 +0200

    fixed a pushed error

--- Agario/Assets/Scripts/PlayerSpawner.cs
+++ Agario/Assets/Scripts/PlayerSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour{
    [SerializeField] GameObject playerPrefab;
    PlayArea area;
-   MatchInfo _matchInfo = new();
+   MatchInfo _matchInfo = new MatchInfo();
    
    bool isAlreadySpawned;
 
