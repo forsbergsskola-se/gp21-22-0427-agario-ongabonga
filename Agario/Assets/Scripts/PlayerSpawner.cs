70a1cfd71a58c8e87236b0ecb104c0997e978479
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-05-12 12:18:36 +0200

    fixed displaying correct score

--- Agario/Assets/Scripts/PlayerSpawner.cs
+++ Agario/Assets/Scripts/PlayerSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour{
    [SerializeField] GameObject playerPrefab;
    PlayArea area;
-   MatchInfo _matchInfo = new MatchInfo();
+   MatchInfo _matchInfo = new();
    
    bool isAlreadySpawned;
 
